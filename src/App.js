e36575473a4af0977a1b4031cbab7cbe6ec53ab2
peyman naderi <peyman.developer.01@gmail.com>
2024-05-04 02:12:34 +0330

    install project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import './index.css'
+function App() {
+  return (
+    <div>
+      <p className='text-red-800 text-center'>peymna</p>
+    </div>
+  );
+}
+
+export default App;
